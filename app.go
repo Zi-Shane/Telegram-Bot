be37a733c7478a191c6c24f8d0341110ffd81ec4
Zi-Shane <shane110319@gmail.com>
2021-11-10 17:49:13 +0800

    Update with ngrok(dev mode)

--- app.go
+++ app.go
@@ -28,14 +28,17 @@ func main() {
 	var webhookUrl string
 	if production {
 		webhookUrl = os.Getenv("DOMAINNAME") + ":" + os.Getenv("PORT") + "/" + bot.Token
-		cert = "./secret/tls.crt"
+		cert = "./secret/" + os.Getenv("DOMAINNAME") + "/tls.crt"
+		_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert(webhookUrl, cert))
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else {
-		webhookUrl = os.Getenv("DOMAINNAME")
-		cert = nil
-	}
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert(webhookUrl, cert))
-	if err != nil {
-		log.Fatal(err)
+		webhookUrl = os.Getenv("DOMAINNAME") + "/" + bot.Token
+		_, err = bot.SetWebhook(tgbotapi.NewWebhook(webhookUrl))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	// Test Webhook
@@ -49,7 +52,15 @@ func main() {
 
 	// Setup Webhook server
 	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS(":"+os.Getenv("PORT"), "./secret/tls.crt", "./secret/tls.key", nil)
+	if production {
+		go http.ListenAndServeTLS(
+			":"+os.Getenv("PORT"),
+			"./secret/"+os.Getenv("DOMAINNAME")+"/tls.crt",
+			"./secret/"+os.Getenv("DOMAINNAME")+"/tls.key",
+			nil)
+	} else {
+		go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	}
 
 	// Get Updates from telegram
 	for update := range updates {
